56214aea1f8ab390a5b6803afc4092dac67bc9a9
ayoubtrabelsitr <ayoubtrabelsitr@gmail.com>
2024-10-13 04:10:20 +0200

    Animals.cpp

---
+++ Session1/Animals.cpp
@@ -0,0 +1,19 @@
+
+#include<format>
+#include<iostream>
+
+using std::format;
+using std::cout;
+using std::string;
+
+//-- interface --
+class Animal {
+	string type{};
+	string name{};
+	string sound{};
+public :
+	Animal();
+	Animal(const string& type, const string& name, const string& sound);
+	Animal(const Animal&);//Copy constructor
+	~Animal();//destructor
+};
